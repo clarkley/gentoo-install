805e48a904983ce5f11ebc1dc383ab21996ce16a
Xianzheng Zhou <xianzheng.zhou@gmail.com>
2018-04-10 19:33:13 +0800

    partition optimal

--- part.sh
+++ part.sh
@@ -4,11 +4,11 @@
 
 disk_path=/dev/$disk_dev
 parted $disk_path mklabel gpt
-parted --align $disk_path mkpart esp fat32 $efi_start $efi_end
+parted --align optimal $disk_path mkpart esp fat32 $efi_start $efi_end
 parted $disk_path name 1 esp
 parted $disk_path set 1 esp on
-parted --align $disk_path mkpart primary $boot_type $boot_start $boot_end
+parted --align optimal $disk_path mkpart primary $boot_type $boot_start $boot_end
 parted $disk_path name 2 boot
-parted --align $disk_path mkpart primary $boot_end 100%
+parted --align optimal $disk_path mkpart primary $boot_end 100%
 parted $disk_path name 3 lvm
 parted $disk_path set 3 lvm on
