063f780fa6920b64e09b7c5e437caab1f9bda4cc
Xianzheng Zhou <xianzheng.zhou@gmail.com>
2018-04-10 19:22:21 +0800

    partition

--- part.sh
+++ part.sh
@@ -2,16 +2,14 @@
 
 . modules/load-config
 
-clearpart --drives=$disk_dev
-
 disk_path=/dev/$disk_dev
-parted $disk_path mklabel gpt \
-    mkpart esp fat32 $efi_start $efi_end \
-    name 1 esp \
-    set 1 esp on \
-    mkpart primary $boot_type $boot_start $boot_end \
-    name 2 boot \
-    set 2 boot on
-    mkpart primary $boot_end 100%
-    name 3 lvm
-    set 3 lvm on
+parted $disk_path mklabel gpt
+parted $disk_path mkpart esp fat32 $efi_start $efi_end
+parted $disk_path name 1 esp
+parted $disk_path set 1 esp on
+parted $disk_path mkpart primary $boot_type $boot_start $boot_end
+parted $disk_path name 2 boot
+parted $disk_path set 2 boot on
+parted $disk_path mkpart primary $boot_end 100%
+parted $disk_path name 3 lvm
+parted $disk_path set 3 lvm on
