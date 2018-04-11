449959db80826df4d6d65c16372d4228b357d2f6
Xianzheng Zhou <xianzheng.zhou@gmail.com>
2018-04-11 13:25:17 +0800

    force formatting

--- install_base.sh
+++ install_base.sh
@@ -4,43 +4,42 @@
 
 # format system folders
 swapoff $lvm_swap
-mkswap $lvm_swap
-
-umount $boot_mount
-mkfs -f -t $boot_type $boot_dev
-
-umount $lvm_root
-mkfs -f -t $lvm_root_type $lvm_root
-
-umount $lvm_home
-mkfs -f -t $lvm_home_type $lvm_home
-
+mkswap -f $lvm_swap
 swapon $lvm_swap
 
-root=/mnt/gentoo
-mkdir -p $root
-mount $lvm_root $root
-mkdir -p $root/tmp
-chmod 1777 $root/tmp
+#format all system disks
+umount $root_mount/proc
+umount $root_mount/sys
+umount $root_mount/sys
+umount $root_mount/dev
+umount $root_mount/dev
+umount $root_mount
+mkfs -t $boot_type -f $boot_dev
+mkfs -t $lvm_root_type -f $lvm_root
+
+mkdir -p $root_mount
+mount $lvm_root $root_mount
+mkdir -p $root_mount/tmp
+chmod 1777 $root_mount/tmp
 
 ntpd -q -g
 
 links https://www.gentoo.org/downloads/mirrors/
 
-tar xpf stage3-*.tar.{bz2,xz} --xattrs-include='*.*' --numeric-owner -C $root
-config_set $root/etc/portage/make.conf CFLAGS "-march=native -O2 -pipe"
-config_set $root/etc/portage/make.conf CXXFLAGS "\${CFLAGS}"
-config_set $root/etc/portage/make.conf MAKEOPTS "-j2"
+tar xpf stage3-*.tar.{bz2,xz} --xattrs-include='*.*' --numeric-owner -C $root_mount
+config_set $root_mount/etc/portage/make.conf CFLAGS "-march=native -O2 -pipe"
+config_set $root_mount/etc/portage/make.conf CXXFLAGS "\${CFLAGS}"
+config_set $root_mount/etc/portage/make.conf MAKEOPTS "-j2"
 
-mirrorselect -i -o >> $root/etc/portage/make.conf
-mkdir -p $root/etc/portage/repos.conf
-cp $root/usr/share/portage/config/repos.conf $root/etc/portage/repos.conf/gentoo.conf
-cp --dereference /etc/resolv.conf $root/etc/
-mount --types proc /proc $root/proc
-mount --rbind /sys $root/sys
-mount --make-rslave $root/sys
-mount --rbind /dev $root/dev
-mount --make-rslave $root/dev
+mirrorselect -i -o >> $root_mount/etc/portage/make.conf
+mkdir -p $root_mount/etc/portage/repos.conf
+cp $root_mount/usr/share/portage/config/repos.conf $root_mount/etc/portage/repos.conf/gentoo.conf
+cp --dereference /etc/resolv.conf $root_mount/etc/
+mount --types proc /proc $root_mount/proc
+mount --rbind /sys $root_mount/sys
+mount --make-rslave $root_mount/sys
+mount --rbind /dev $root_mount/dev
+mount --make-rslave $root_mount/dev
 
-cp -r . $root/root/install
-chroot $root /bin/bash
+cp -r . $root_mount/root/install
+chroot $root_mount /bin/bash
