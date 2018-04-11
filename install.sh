ff21e962157b7d2718f5298d13141e7fbc182c41
Xianzheng Zhou <xianzheng.zhou@gmail.com>
2018-04-11 10:39:30 +0800

    sed compatibility

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ links https://www.gentoo.org/downloads/mirrors/
 
 tar xpf stage3-*.tar.{bz2,xz} --xattrs-include='*.*' --numeric-owner -C $root
 config_set $root/etc/portage/make.conf CFLAGS "-march=native -O2 -pipe"
-config_set $root/etc/portage/make.conf CXXFLAGS "\$\{CFLAGS\}"
+config_set $root/etc/portage/make.conf CXXFLAGS "\${CFLAGS}"
 config_set $root/etc/portage/make.conf MAKEOPTS "-j2"
 
 mirrorselect -i -o >> $root/etc/portage/make.conf
