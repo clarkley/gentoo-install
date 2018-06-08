6436bb94819acb25388082927cff69f8a8b6204e
Xianzheng Zhou <xianzheng.zhou@gmail.com>
2018-06-08 11:35:56 +0800

    update all packages during installation

--- 3_system.sh
+++ 3_system.sh
@@ -57,7 +57,7 @@ done
 
 emerge app-portage/cpuid2cpuflags
 echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
-emerge --changed-use --deep --with-bdeps=y @world
+emerge --update --changed-use --deep --with-bdeps=y @world
 
 # kernel
 conf=/usr/src/linux/.config
